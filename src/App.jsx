587f9f7d2317b87d92010c1d631c53e087d42220
Jose <joseluissequeirag2@gmail.com>
2024-09-18 14:05:58 -0600

    quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Routes, Route, Navigate, useLocation, Outlet } from "react-router-dom";
+
 import "./App.css";
-import Login from "./Pages/Login/Login";
+
 import IndexLanding from "./pages/Index/IndexLanding";
 function App() {
   const [count, setCount] = useState(0);
