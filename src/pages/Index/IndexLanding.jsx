ca654e12afb8e8261b4a048d5cb58f8b0deb8bb4
Jose <joseluissequeirag2@gmail.com>
2024-09-21 23:16:38 -0600

    QUICKFIX

--- src/pages/Index/IndexLanding.jsx
+++ src/pages/Index/IndexLanding.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigator from "../../components/Nav/Nav";
 import Home from "./IndexSection/Home";
-import Features from "./IndexSection/features";
+import Features from "./IndexSection/Features";
 export default function IndexLanding() {
   return (
     <>
--- src/pages/Index/IndexSection/Features.jsx
+++ src/pages/Index/IndexSection/Features.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Reveal from "../../../components/Reveal";
-export default function features() {
+export default function Features() {
   return (
     <div className="h-[100vh] w-[100vw] flex justify-center items-start ">
       <div className="mt-10 bg-red-50">
